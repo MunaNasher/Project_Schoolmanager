55cf17369d3a44775754a95a0162654228682e40
Muna Nasher <munanasher@gmail.com>
2026-02-23 22:50:55 +0100

    Complete course management functionality

--- src/traineeCommands.js
+++ src/traineeCommands.js
@@ -1,25 +1,121 @@
-import { saveTraineeData, loadTraineeData } from './storage.js';
+import { saveTraineeData, loadTraineeData, loadCourseData, saveCourseData } from './storage.js';
 
-function addTrainee() {
-  // TODO: Implement the logic
+function generateId() {
+  return Math.floor(Math.random() * 100000);
 }
 
-function updateTrainee() {
-  // TODO: Implement the logic
+// add
+function addTrainee(args) {
+  const [firstName, lastName] = args;
+
+  if (!firstName || !lastName) {
+    console.log('Please provide first and last name.');
+    return;
+  }
+
+  const trainees = loadTraineeData();
+
+  const newTrainee = {
+    id: generateId(),
+    firstName,
+    lastName
+  };
+
+  trainees.push(newTrainee);
+  saveTraineeData(trainees);
+
+  console.log('Trainee added successfully:', newTrainee);
 }
 
-function deleteTrainee() {
-  // TODO: Implement the logic
+// update
+function updateTrainee(args) {
+  const [id, newFirstName, newLastName] = args;
+
+  if (!id) {
+    console.log('Please provide trainee ID.');
+    return;
+  }
+
+  const trainees = loadTraineeData();
+  const trainee = trainees.find(t => t.id == id);
+
+  if (!trainee) {
+    console.log('Trainee not found.');
+    return;
+  }
+
+  if (newFirstName) trainee.firstName = newFirstName;
+  if (newLastName) trainee.lastName = newLastName;
+
+  saveTraineeData(trainees);
+
+  console.log('Trainee updated successfully.');
 }
 
-function fetchTrainee() {
-  // TODO: Implement the logic
+// delete
+function deleteTrainee(args) {
+  const [id] = args;
+
+  if (!id) {
+    console.log('Please provide trainee ID.');
+    return;
+  }
+
+  // remove trainee from trainee list
+  const trainees = loadTraineeData();
+  const updatedTrainees = trainees.filter(t => t.id != id);
+  saveTraineeData(updatedTrainees);
+
+  // remove trainee from all courses
+  const courses = loadCourseData();
+  courses.forEach(course => {
+    course.participants = course.participants.filter(pid => pid != id);
+  });
+  saveCourseData(courses);
+
+  console.log('Trainee deleted successfully and removed from all courses.');
 }
 
+// get one
+function fetchTrainee(args) {
+  const [id] = args;
+
+  const trainees = loadTraineeData();
+  const trainee = trainees.find(t => t.id == id);
+
+  if (!trainee) {
+    console.log('Trainee not found.');
+    return;
+  }
+
+  console.log(trainee);
+}
+
+// get all
 function fetchAllTrainees() {
-  // TODO: Implement the logic
+  const trainees = loadTraineeData();
+  console.log(trainees);
 }
 
+// handel
 export function handleTraineeCommand(subcommand, args) {
-  // Read the subcommand and call the appropriate function with the arguments
+  switch (subcommand) {
+    case 'ADD':
+      addTrainee(args);
+      break;
+    case 'UPDATE':
+      updateTrainee(args);
+      break;
+    case 'DELETE':
+      deleteTrainee(args);
+      break;
+    case 'GET':
+      fetchTrainee(args);
+      break;
+    case 'LIST':
+      fetchAllTrainees();
+      break;
+    default:
+      console.log('Unknown trainee command.');
+  }
 }
